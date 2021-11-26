3d5c4f774e435128460413ae323018e9c461bc61
Aleksandrs Olins <92582420+DungeonMastr@users.noreply.github.com>
2021-11-26 18:12:20 +0200

    Update form.py

--- form.py
+++ form.py
@@ -1,24 +1,22 @@
-from bottle import Bottle, request
+from flask import Flask 
+from flask import request 
 import csv
 
-app = Bottle()
-##Should pull out info but idk
-@app.route('/form.py', method="POST")
-def formhandler():
-
-  name = request.forms.get('fname')
-  surname = request.forms.get('fsurname')
-  company_name = request.forms.get('fcompany')
-  company_desc = request.forms.get('fcomment')
-  phone_num = request.forms.get('fnumber')
-  email = request.forms.get('femail')
-  full_info = name + ',' + surname + ',' + company_name + ',' + company_desc + ',' + phone_num + ',' + email + '\n'
+def login() 
+	if request.method == 'POST': 
+		name = request.form['fname'] 
+		surname = request.form['fsurname']
+    companyName = request.form['fcompany']
+    companyDesc = request.form['fcomment']
+    email = request.form['femail']
+    number = request.form['fnumber']
+  full_info = name + ',' + surname + ',' + companyName + ',' + companyDesc + ',' + email + ',' + number 
   
-  fname = 'surveyinfo.csv'
-  with open('path/to/csv_file', 'w') as f:
+  fname = "surveyinfo.csv"
+  
+  with open(fname, 'w') as f:
     writer = csv.writer(f)
     writer.writerow(full_info)
-    
     
      
     
