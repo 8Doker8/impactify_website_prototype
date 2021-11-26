8f09fcf0fdf259ac740f2035dd4459089c56d352
Aleksandrs Olins <92582420+DungeonMastr@users.noreply.github.com>
2021-11-26 18:16:19 +0200

    Update form.py

--- form.py
+++ form.py
@@ -6,17 +6,17 @@ def login()
 	if request.method == 'POST': 
 		name = request.form['fname'] 
 		surname = request.form['fsurname']
-    companyName = request.form['fcompany']
-    companyDesc = request.form['fcomment']
-    email = request.form['femail']
-    number = request.form['fnumber']
-  full_info = name + ',' + surname + ',' + companyName + ',' + companyDesc + ',' + email + ',' + number 
+    		companyName = request.form['fcompany']
+    		companyDesc = request.form['fcomment']
+    		email = request.form['femail']
+    		number = request.form['fnumber']
+  	full_info = name + ',' + surname + ',' + companyName + ',' + companyDesc + ',' + email + ',' + number 
   
-  fname = "surveyinfo.csv"
+  	fname = "surveyinfo.csv"
   
-  with open(fname, 'w') as f:
-    writer = csv.writer(f)
-    writer.writerow(full_info)
+  	with open(fname, 'w') as f:
+    		writer = csv.writer(f)
+    		writer.writerow(full_info)
     
      
     
